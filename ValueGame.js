22237ce0d39ebf6e93322a44bf2680592a9b0c1b
ian6207 <ianv6207@mytusd.org>
2025-10-23 10:12:41 -0700

    Add player prompts and alert for Value Game

--- ValueGame.js
+++ ValueGame.js
@@ -1,1 +1,4 @@
 //To play Value Game you must enter as much letters or numbers as you can. The player with the most value wins.
+var Player1 = prompt("Player 1");
+var Player2 = prompt("Player 2");
+alert("Click close when you are ready to reaveal the winner.");
