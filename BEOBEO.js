3ecc1ea7e82f576884575fb55362a097c9a3392d
ian6207 <ianv6207@mytusd.org>
2025-10-28 19:56:56 -0700

    Fix method to get elements by tag name

--- BEOBEO.js
+++ BEOBEO.js
@@ -1,2 +1,3 @@
+//Enter tag's name and will change the element to: "BEO, BEO, BEO, BEO."
 var That = prompt("Enter Element:");
-document.getElementsByElement(That)[0].innerText = "BEO, BEO, BEO, BEO.";
+document.getElementsByTagName(That)[0].innerText = "BEO, BEO, BEO, BEO.";
