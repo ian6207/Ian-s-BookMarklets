a7d93009860f54f1aa313dc784eddfcc930ca0fe
ian6207 <ianv6207@mytusd.org>
2025-11-02 19:08:11 -0800

    Create CreateWebPage.js to insert HTML into a page

---
+++ CreateWebPage.js
@@ -0,0 +1,5 @@
+//Go to about:blank and run the bookmarklet. When it runs it will ask for HTML then insert the HTML into the page.
+var inner = prompt("HTML");
+const WebPage = document.createElement('div');
+WebPage.innerHTML = inner;
+document.appendChild("inner");
