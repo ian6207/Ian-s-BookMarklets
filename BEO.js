beb5f531b3ae16b64032675dc93fe763289c74d5
ian6207 <ianv6207@mytusd.org>
2025-12-21 08:42:32 -0800

    Combine variable declaration and assignment

--- BEO.js
+++ BEO.js
@@ -1,3 +1,2 @@
 //Ask for id, then replace id with BEO
-var ChangeId = prompt("Id?");
-document.getElementById(ChangeId).innerHTML = "BEO";
+var ChangeId = prompt("Id?"); document.getElementById(ChangeId).innerHTML = "BEO";
