a8439807ab3268468a49c2b7973582dc1df733be
ian6207 <ianv6207@mytusd.org>
2025-12-21 09:05:06 -0800

    Replace all images on page with Doge image
    This script changes all images on the page to a specific Doge image.

--- DogeInvade.js
+++ DogeInvade.js
@@ -1,1 +1,2 @@
+//Changes all images on page to Kabosu
 javascript:var Kids = document.getElementsByTagName("img"); for (let Teacher of Kids) {Teacher.src = "https://static.wikia.nocookie.net/sanicsource/images/9/97/Doge.jpg";}
