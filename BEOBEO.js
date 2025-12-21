a448794b02e6130e5a7c648a31506115cc7697bc
ian6207 <ianv6207@mytusd.org>
2025-12-21 08:38:20 -0800

    Change innerText to textContent in BEOBEO.js
    Updated the method to change inner text to text content.

--- BEOBEO.js
+++ BEOBEO.js
@@ -1,3 +1,2 @@
 //Enter tag's name and will change the element to: "BEO, BEO, BEO, BEO."
-var That = prompt("Enter Element:");
-document.getElementsByTagName(That)[0].innerText = "BEO, BEO, BEO, BEO.";
+javascript:var That = prompt("Enter Element:"); document.getElementsByTagName(That)[0].textContent = "BEO, BEO, BEO, BEO."
