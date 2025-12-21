16a2ee0dd9758798c15c6eada59103ec31c0cd5e
ian6207 <ianv6207@mytusd.org>
2025-12-21 08:39:21 -0800

    Refactor CreateWebPage.js to streamline HTML insertion

--- CreateWebPage.js
+++ CreateWebPage.js
@@ -1,5 +1,2 @@
 //Go to about:blank and run the bookmarklet. When it runs it will ask for HTML then insert the HTML into the page.
-var inner = prompt("HTML");
-const WebPage = document.createElement('div');
-WebPage.innerHTML = inner;
-document.body.appendChild("inner");
+javascript:var inner = prompt("HTML"); const WebPage = document.createElement('div'); WebPage.innerHTML = inner; document.body.appendChild("inner");
